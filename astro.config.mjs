63769c6d01fa2b181b7088031fd903a6b23cd997
Natnicha Inthong <gam110507.2@gmail.com>
2025-08-12 15:20:21 +0700

    change to astro framework

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+    site: 'https://gam110507.github.io',
+    base: 'my-webpage',
+
+});
